9c85c00e4b34165b76bb67e90018f54643ecb8a8
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-26 13:01:44 +0530

    first snippet

---
+++ java_interview_pratice/src/com/learn/java_interview_pratice/helloworld.java
@@ -0,0 +1,11 @@
+package com.learn.java_interview_pratice;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("heeelo world welcome ");
+System.out.println("hello interviewer");
+	}
+
+}
